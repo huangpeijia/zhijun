da0df2764fbb624eafc6c9892963bad0c386ba25
huangpeijia <1297382876@qq.com>
2018-08-21 13:39:40 +0800

    类型管理

--- ZhiJun_dev/src/main/java/com/zhijun/dao/ProtypeDao.java
+++ ZhiJun_dev/src/main/java/com/zhijun/dao/ProtypeDao.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
+import com.zhijun.bean.Newtype;
 import com.zhijun.bean.Protype;
 
 public class ProtypeDao {
@@ -20,11 +21,11 @@ public class ProtypeDao {
 			this.jdbcTemplate = jdbcTemplate;
 		}
 	/**
-	 * 查询所有产品类型
+	 * 查询所有产品
 	 * @return
 	 */
-	public List<Protype> queryAll(){
-		String sql ="select protype_id,protype_name from protype";
+	public List<Protype> queryAll(int pages, int keys){
+		String sql ="select protype_id,protype_name from protype limit "+pages+","+keys+"";
 		List<Protype> list = jdbcTemplate.query(sql,new ProtypeMapper());
 		return list;
 		
@@ -40,6 +41,29 @@ public class ProtypeDao {
 		return list;
 		
 	}
+	//查询数量
+	public int countAll() {
+		String sql = "select count(*) from protype";
+		Integer count =jdbcTemplate.queryForObject(sql , Integer.class);  
+		return count;
+		
+	}
+	//添加
+		public int add(Protype protype) {
+			String sql = "insert into protype(protype_name) value(?)";
+			return jdbcTemplate.update(sql,new Object[] {protype.getProtype_name()});
+		}
+		//删除
+		public int delete(int protype_id) {
+			String sql = "delete from protype where protype_id=?";
+			return jdbcTemplate.update(sql, new Object[] {protype_id});
+		}	
+		
+		//修改
+		public int update(Protype protype) {
+			String sql = "update protype set protype_name=? where protype_id=?";
+			return jdbcTemplate.update(sql,new Object[] {protype.getProtype_name(),protype.getProtype_id()});
+		}
 	public class ProtypeMapper implements RowMapper<Protype>{
 		public Protype mapRow(ResultSet rs, int rowNum) throws SQLException{
 			Protype protype= new Protype(); 
